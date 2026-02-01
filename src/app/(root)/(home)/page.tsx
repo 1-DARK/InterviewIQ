90c61bcdf807089897ca496821f6579271d5eca3
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-01 23:48:17 +0530

    update home page with four components

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,8 +1,12 @@
 "use client";
+import ActionCard from "@/components/ActionCard";
+import { QUICK_ACTIONS } from "@/constants";
 import { useUserRole } from "@/hooks/useUserRole";
 
 export default function Home() {
   const { isInterviewer, isCandidate, isLoading } = useUserRole();
+
+  const handleQuickAction = (title: string) => {};
   return (
     <div className="container max-w-7xl mx-auto p-6">
       <div className="rounded-lg bg-card p-6 border shadow-sm mb-10">
@@ -16,7 +20,15 @@ export default function Home() {
 
       {isInterviewer ? (
         <>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">hello</div>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {QUICK_ACTIONS.map((action) => (
+              <ActionCard
+                key={action.title}
+                action={action}
+                onClick={() => handleQuickAction(action.title)}
+              />
+            ))}
+          </div>
         </>
       ) : (
         <>
