e807b0f49d569f28c18cd3b9ff714e727772bc4d
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-03 23:39:21 +0530

    update stream provider

--- src/components/providers/StreamClientProvider.tsx
+++ src/components/providers/StreamClientProvider.tsx
@@ -1,4 +1,17 @@
 "use client";
-const StreamClientProvider = () => {};
+
+import { useUser } from "@clerk/nextjs";
+import { StreamVideoClient } from "@stream-io/node-sdk";
+import { ReactNode, useEffect, useState } from "react";
+
+const StreamClientProvider = ({ children }: { children: ReactNode }) => {
+  const [streamVideoClient, setStreamVideoClient] =
+    useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (!user || !isLoaded) return;
+  }, [user, isLoaded]);
+};
 
 export default StreamClientProvider;
