cd1ceb93277b14a4c8009361f546e3f1fb3cf9e5
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-04 23:05:30 +0530

    init meeting page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,17 @@
+"use client";
+
+import LoaderUI from "@/components/LoaderUI";
+import { useUser } from "@clerk/nextjs";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+
 function MeetingPage() {
+  const { id } = useParams();
+  const { isLoaded } = useUser();
+  const isCallLoading = true;
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <LoaderUI />;
   return <div></div>;
 }
 
