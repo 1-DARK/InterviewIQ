df19a8d53f5e4e2be1e5c30e2fade519e3128b99
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-05 22:53:54 +0530

    init camera on/off functionality

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,5 +1,29 @@
-function MeetingSetup() {
-  return <div>hello</div>;
+import { useCall } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
+function MeetingSetup({ onSetupComplete }: { onSetupComplete: () => void }) {
+  const [isCameraDisabled, setIsCameraDisabled] = useState(true);
+  const [isMicDisabled, setIsMicDisabled] = useState(false);
+
+  const call = useCall();
+
+  if (!call) return null;
+
+  useEffect(() => {
+    if (isCameraDisabled) call.camera.disable();
+    else call.camera.enable();
+  }, [isCameraDisabled, call.camera]);
+
+  useEffect(() => {
+    if (isMicDisabled) call.microphone.disable();
+    else call.microphone.enable();
+  }, [isMicDisabled, call.microphone]);
+
+  const handleJoin = async () => {
+    await call.join();
+    onSetupComplete();
+  };
+  return <div></div>;
 }
 
 export default MeetingSetup;
