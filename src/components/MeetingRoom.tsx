f10bf19a41eb0575541dc6bba2b8f4371717a9b0
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-06 23:16:55 +0530

    init meeting room

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,4 +1,23 @@
+import { CallingState, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { LoaderIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 function MeetingRoom() {
+  const router = useRouter();
+  const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+
+  const callingState = useCallCallingState(); // use to check for join or not
+
+  if (callingState !== CallingState.JOINED) {
+    return (
+      <div className="h-96 flex items-center justify-center">
+        <LoaderIcon className="size-6 animate-spin" />
+      </div>
+    );
+  }
   return <div>hi</div>;
 }
 
