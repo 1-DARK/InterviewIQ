e6b6b085b161f7df01548f9cba33af419a01bd4d
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-07 23:34:14 +0530

    init language and ques selection component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,5 +1,14 @@
 import { CODING_QUESTIONS, LANGUAGES } from "@/constants";
 import { useState } from "react";
+import { ResizablePanel, ResizablePanelGroup } from "./ui/resizable";
+import { ScrollArea } from "./ui/scroll-area";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 function CodeEditor() {
   const [selectedQuestion, setSelectedQuestion] = useState(CODING_QUESTIONS[0]);
@@ -22,7 +31,84 @@ function CodeEditor() {
     setLanguage(newLanguage);
     setCode(selectedQuestion.starterCode[newLanguage]);
   };
-  return <div>hello</div>;
+  return (
+    <ResizablePanelGroup
+      orientation="vertical"
+      className="min-h-[calc-100vh-4rem-1px]"
+    >
+      {/* questions */}
+      <ResizablePanel>
+        <ScrollArea className="h-full">
+          <div className="p-6">
+            <div className="max-w-4xl mx-auto space-y-6">
+              <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+                <div className="space-y-1">
+                  <div className="flex items-center gap-2">
+                    <h2 className="text-2xl font-semibold tracking-tight">
+                      {selectedQuestion.title}
+                    </h2>
+                  </div>
+                  <p className="text-sm text-muted-foreground">
+                    Choose your language and solve the problem
+                  </p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Select
+                    value={selectedQuestion.id}
+                    onValueChange={handleQuestionChange}
+                  >
+                    <SelectTrigger className="w-45">
+                      <SelectValue placeholder="Select question" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {CODING_QUESTIONS.map((q) => (
+                        <SelectItem key={q.id} value={q.id}>
+                          {q.title}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+
+                  <Select value={language} onValueChange={handleLanguageChange}>
+                    <SelectTrigger className="w-37.5">
+                      {/* value */}
+                      <SelectValue>
+                        <div className="flex items-center gap-2">
+                          <img
+                            src={`/${language}.png`}
+                            alt={language}
+                            className="w-5 h-5 object-contain"
+                          />
+                          {LANGUAGES.find((l) => l.id === language)?.name}
+                        </div>
+                      </SelectValue>
+                    </SelectTrigger>
+                    {/* content */}
+                    <SelectContent>
+                      {LANGUAGES.map((lang) => (
+                        <SelectItem key={lang.id} value={lang.id}>
+                          <div className="flex items-center gap-2">
+                            <img
+                              src={`/${lang.id}.png`}
+                              alt={lang.name}
+                              className="w-5 h-5 object-contain"
+                            />
+                            {lang.name}
+                          </div>
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
+      </ResizablePanel>
+      {/* code editor  */}
+      <ResizablePanel></ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
 
 export default CodeEditor;
