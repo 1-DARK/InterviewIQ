f9be44648f188377cf25c70c1536eff2e6438cf8
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-07 22:35:42 +0530

    update the endcall component

--- src/components/EndCallButton.tsx
+++ src/components/EndCallButton.tsx
@@ -2,6 +2,8 @@ import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
 import { useMutation, useQuery } from "convex/react";
 import { useRouter } from "next/navigation";
 import { api } from "../../convex/_generated/api";
+import { Button } from "./ui/button";
+import toast from "react-hot-toast";
 
 function EndCallButton() {
   const call = useCall();
@@ -18,7 +20,30 @@ function EndCallButton() {
   });
 
   if (!call || !interview) return null;
-  return <div>hello</div>;
+
+  const isMeetingOwner = localParticipant?.userId === call.state.createdBy?.id;
+
+  if (!isMeetingOwner) return null;
+
+  const endCall = async () => {
+    try {
+      await call.endCall();
+      await updateInterviewStatus({
+        id: interview._id,
+        status: "completed",
+      });
+      router.push("/");
+      toast.success("Meeting Ended for everyone");
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to end meeting");
+    }
+  };
+  return (
+    <Button variant={"destructive"} onClick={endCall}>
+      End Meeting
+    </Button>
+  );
 }
 
 export default EndCallButton;
