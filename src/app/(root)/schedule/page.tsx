773b9db95fbbf6d63d91032cc7fddfb5fa921694
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-16 23:18:55 +0530

    init schedule page

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+import LoaderUI from "@/components/LoaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
+
 function page() {
+  const router = useRouter();
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if (isLoading) return <LoaderUI />;
   return <div>schedule</div>;
 }
 
