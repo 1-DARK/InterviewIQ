12aface5fea052fa21f675990e03ee367ce64d20
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-16 22:45:49 +0530

    init fetch recordings

--- src/app/(root)/recordings/page.tsx
+++ src/app/(root)/recordings/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
+import LoaderUI from "@/components/LoaderUI";
 import useGetCalls from "@/hooks/useGetCalls";
+import { CallRecording } from "@stream-io/video-react-sdk";
+
+import { useEffect, useState } from "react";
 
 function RecordingPage() {
   const { calls, isLoading } = useGetCalls();
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      if (!calls) return;
+      try {
+        // recordings for each call
+        const callData = await Promise.all(
+          calls.map((call) => call.queryRecordings()),
+        );
+        const allRecordings = callData.flatMap((call) => call.recordings);
+        setRecordings(allRecordings);
+      } catch (error) {
+        console.log("Error fetching recordings:", error);
+      }
+    };
+    fetchRecordings();
+  }, [calls]);
+
+  if (isLoading) return <LoaderUI />;
   return <div>record</div>;
 }
 
