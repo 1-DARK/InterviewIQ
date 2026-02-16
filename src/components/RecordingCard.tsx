669bea35f5ed2a32a6184b96c5c08e1010bba8bf
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-16 22:58:40 +0530

    init recordingcard

---
+++ src/components/RecordingCard.tsx
@@ -0,0 +1,8 @@
+import { CallRecording } from "@stream-io/video-react-sdk";
+import React from "react";
+
+function RecordingCard({ recording }: { recording: CallRecording }) {
+  return <div>hi</div>;
+}
+
+export default RecordingCard;
