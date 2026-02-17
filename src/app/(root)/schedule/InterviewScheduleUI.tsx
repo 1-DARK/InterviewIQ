88bbdbc6cd5654a988bc3cd9909a7b7ddb55a3bc
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-17 23:01:43 +0530

    init form func

--- src/app/(root)/schedule/InterviewScheduleUI.tsx
+++ src/app/(root)/schedule/InterviewScheduleUI.tsx
@@ -3,6 +3,7 @@ import { useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useMutation, useQuery } from "convex/react";
 import { useState } from "react";
 import { api } from "../../../../convex/_generated/api";
+import toast from "react-hot-toast";
 
 function InterviewScheduleUI() {
   const client = useStreamVideoClient();
@@ -12,6 +13,27 @@ function InterviewScheduleUI() {
   const interviews = useQuery(api.interview.getAllInterviews);
   const users = useQuery(api.users.getUsers);
   const createInterview = useMutation(api.interview.createInterview);
+
+  const candidates = users?.filter((u) => u.role === "candidate");
+  const interviewers = users?.filter((u) => u.role === "interviewer");
+
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    date: new Date(),
+    time: "09:00",
+    candidateId: "",
+    interviewerIds: user?.id ? [user.id] : [],
+  });
+
+  const scheduleMeeting = async () => {
+    if (!client || !user) return;
+    if (!formData.candidateId || formData.interviewerIds.length === 0) {
+      toast.error("Please select both candidate and at least one interviewer");
+      return;
+    }
+  };
+
   return <div>hi schedule</div>;
 }
 
