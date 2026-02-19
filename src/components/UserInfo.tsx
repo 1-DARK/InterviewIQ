7386d761a9859ec1cb5a9380c19a1f85563fe687
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-19 22:10:15 +0530

    add a functionality

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -7,12 +7,6 @@ type User = Doc<"users">;
 function UserInfo({ user }: { user: User }) {
   return (
     <div className="flex items-center gap-2">
-      <Avatar className="h-6 w-6">
-        <AvatarImage src={user.image} />
-        <AvatarFallback>
-          <UserCircleIcon className="h-4 w-4" />
-        </AvatarFallback>
-      </Avatar>
       <span>{user.name}</span>
     </div>
   );
