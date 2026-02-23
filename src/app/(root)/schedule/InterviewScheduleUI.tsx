3ddacf5048b4804621b3ce45ebdc37190995a4a2
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-02-23 22:17:09 +0530

    fix issue in jwt domain token

--- src/app/(root)/schedule/InterviewScheduleUI.tsx
+++ src/app/(root)/schedule/InterviewScheduleUI.tsx
@@ -261,21 +261,6 @@ function InterviewScheduleUI() {
 
               <div className="space-y-2">
                 <label className="text-sm font-medium">Time</label>
-                <Select
-                  value={formData.time}
-                  onValueChange={(time) => setFormData({ ...formData, time })}
-                >
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select time" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {TIME_SLOTS.map((time) => (
-                      <SelectItem key={time} value={time}>
-                        {time}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
               </div>
             </div>
           </DialogContent>
