da8440c3aff220fed79be1f0cff47e821ce7ed03
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-03-16 22:49:57 +0530

    init Meeting card

--- src/app/(root)/schedule/InterviewScheduleUI.tsx
+++ src/app/(root)/schedule/InterviewScheduleUI.tsx
@@ -305,7 +305,7 @@ function InterviewScheduleUI() {
         <div className="spacey-4">
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
             {interviews.map((interview) => (
-              <MeetingCard key={interview._id} />
+              <MeetingCard key={interview._id} interview={interview} />
             ))}
           </div>
         </div>
--- src/components/MeetingCard.tsx
+++ src/components/MeetingCard.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import useMeetingActions from "@/hooks/useMeetingActions";
+import { Doc } from "../../convex/_generated/dataModel";
+import { getMeetingStatus } from "@/lib/utils";
+import { format } from "date-fns";
 
-function MeetingCard() {
+type Interview = Doc<"interviews">;
+function MeetingCard({ interview }: { interview: Interview }) {
+  const { joinMeeting } = useMeetingActions();
+  const status = getMeetingStatus(interview);
+
+  const formattedDate = format(
+    new Date(interview.startTime),
+    "EEEE, MMMM d · h:mm a",
+  );
   return <div>hi</div>;
 }
 
