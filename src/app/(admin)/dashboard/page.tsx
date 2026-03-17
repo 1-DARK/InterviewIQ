2cc88d643e0181a1be4921dc206a5fc95b1dceb8
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-03-17 22:51:33 +0530

    minor changes in ui

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -3,6 +3,10 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import toast from "react-hot-toast";
 import { Id } from "../../../../convex/_generated/dataModel";
+import LoaderUI from "@/components/LoaderUI";
+import { groupInterviews } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 function DashboardPage() {
   const users = useQuery(api.users.getUsers);
@@ -20,7 +24,20 @@ function DashboardPage() {
       toast.error("Failed to update status");
     }
   };
-  return <div>Dash</div>;
+
+  if (!interviews || !users) return <LoaderUI />;
+
+  const groupedInterviews = groupInterviews(interviews);
+
+  return (
+    <div className=" container mx-auto py-10">
+      <div className="flex items-center mb-8">
+        <Link href="/schedule">
+          <Button>Schedule New Interview</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardPage;
