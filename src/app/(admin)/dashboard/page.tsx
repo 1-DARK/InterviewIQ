624fccd53fa49af1bc9a728a98cc097645489c2c
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-03-17 22:46:35 +0530

    update status of interview

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,9 +1,25 @@
 "use client";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import toast from "react-hot-toast";
+import { Id } from "../../../../convex/_generated/dataModel";
 
 function DashboardPage() {
   const users = useQuery(api.users.getUsers);
+  const interviews = useQuery(api.interview.getAllInterviews);
+  const updateStatus = useMutation(api.interview.updateInterviewStatus);
+
+  const handleStatusUpdate = async (
+    interviewId: Id<"interviews">,
+    status: string,
+  ) => {
+    try {
+      await updateStatus({ id: interviewId, status });
+      toast.success(`Interview marked as ${status}`);
+    } catch (error) {
+      toast.error("Failed to update status");
+    }
+  };
   return <div>Dash</div>;
 }
 
